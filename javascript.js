59e9be992c9b39818b12db8cbdefc67195cb7ccc
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-05-23 12:22:15 +0200

    color for the line and winner

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	const playerX = "X";
 	const playerO = "O";
 	let actualPlayer = playerX;
+	const message = document.querySelector(".win-msg");
+	const winnerName = document.querySelector(".winner-name");
+	const button = document.querySelector(".button");
 
 	const winnerCombinations = [
 		[0, 1, 2],
@@ -22,7 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
 			const index = parseInt(box.getAttribute("data-index"));
 
 			if (box.textContent === "") {
-				box.textContent = actualPlayer;
+				box.innerHTML = `<span class="${actualPlayer.toLowerCase()}">${actualPlayer}</span>`;
+
 				tableState[index] = actualPlayer;
 
 				if (actualPlayer === "X") {
@@ -37,13 +41,46 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 
 	function lookingWinner() {
+		boxes.forEach(box => box.classList.remove("winner"));
+
 		for (let comb of winnerCombinations) {
 			const [a, b, c] = comb;
 
 			if (tableState[a] === tableState[b] && tableState[a] === tableState[c] && tableState[a] !== null) {
-				// Alguien ha ganado
-				console.log(`¡Ha ganado ${tableState[a]}!`);
+				winnerName.textContent = tableState[a];
+
+				boxes[a].classList.add("winner");
+				boxes[b].classList.add("winner");
+				boxes[c].classList.add("winner");
+
+				applyWinAnimation(a);
+				applyWinAnimation(b);
+				applyWinAnimation(c);
+
+				message.classList.remove("hidden");
+				break;
 			}
 		}
 	}
+
+	function applyWinAnimation(index) {
+		const span = boxes[index].querySelector("span");
+		span.classList.remove("winner-animation");
+		void span.offsetWidth;
+		span.classList.add("winner-animation");
+	}
+
+	function resetGame() {
+		tableState = Array(9).fill(null);
+
+		actualPlayer = "X";
+
+		boxes.forEach((box) => {
+			box.textContent = "";
+		});
+
+		message.classList.add("hidden");
+	}
+
+	button.addEventListener("click", resetGame);
 });
